e26b3e6336756664fd3e7e8091a5a95d255c2653
MingYangPeterPan <17010282173@163.com>
2018-06-12 23:24:17 +0800

    Task SimonPan #comment Add weUi and font-awesome

--- server/controllers/demo.js
+++ server/controllers/demo.js
@@ -2,6 +2,6 @@ const { mysql } = require('../qcloud')
 
 module.exports = ctx => {
   var id = 2
-  var res = await mysql("goods_label").where({ id }).first()
+  // var res = await mysql("goods_label").where({ id }).first()
   ctx.state.data = "OK"
 }
