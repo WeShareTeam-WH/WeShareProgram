079ee87f8afb0571a4ee1c11e8716ff13a803aa7
MingYangPeterPan <17010282173@163.com>
2018-06-19 23:23:57 +0800

    Task SimonPan #comment Add a goods to shoppping cart

--- client/pages/goodsDetail/goodsDetail.js
+++ client/pages/goodsDetail/goodsDetail.js
@@ -15,9 +15,10 @@ Page({
     previousMargin: 0,
     nextMargin: 0,
     tabs: ["图文详情", "评论", "商品参数"],
-    activeIndex: 0,
+    activeIndex: 1,
     sliderOffset: 0,
-    sliderLeft: 0
+    sliderLeft: 0,
+    isLike: true,
   },
 
   /**
@@ -39,54 +40,88 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   },
   tabClick: function (e) {
     this.setData({
       sliderOffset: e.currentTarget.offsetLeft,
       activeIndex: e.currentTarget.id
     });
+  },
+  //预览图片
+  previewImage: function (e) {
+    var current = e.target.dataset.src;
+
+    wx.previewImage({
+      current: current, // 当前显示图片的http链接  
+      urls: this.data.background // 需要预览的图片http链接列表  
+    })
+  },
+  // 收藏
+  addLike() {
+    this.setData({
+      isLike: !this.data.isLike
+    });
+  },
+  // 跳到购物车
+  toCar() {
+    wx.switchTab({
+      url: '../shoppingcar/shoppingcar'
+    })
+  },
+  // 立即购买
+  immeBuy() {
+    wx.showToast({
+      title: '购买成功',
+      icon: 'success',
+      duration: 2000
+    });
+  },
+  addCar: function () {
+    wx.navigateTo({
+      url: '../addCar/addCar'
+    })
   }
 })
